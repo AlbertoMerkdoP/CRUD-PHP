5e61fc5e86648ff467821ee11327b5fd094ab186
AlbertoMerkdoP <jacksoon2004@gmail.com>
2023-08-14 23:03:58 -0500

    Cambiar forma de verificar rol de usuario

--- config/Authenticate.php
+++ config/Authenticate.php
@@ -1,39 +1,42 @@
 <?php
 
-    include_once("DBConect.php");
-    session_start();
+	include_once("DBConect.php");
+	include_once('Config.php');
+	session_start();
 
 	$username = $password = "";
-	
-	if(isset($_POST['email'])) $username = $_POST['email']; 
-	if(isset($_POST['password'])) $password = $_POST['password'];
-	
-    $conexion = new Database;  
-    $result = $conexion->DatosAutenticacion($username,$password);
-    $cantidad = $result->rowCount();
 
-    if($cantidad == 0){
+	if (isset($_POST['email']))
+		$username = $_POST['email'];
+	if (isset($_POST['password']))
+		$password = $_POST['password'];
+
+	$conexion = new Database;
+	$result = $conexion->DatosAutenticacion($username, $password);
+	$cantidad = $result->rowCount();
+
+	if ($cantidad == 0) {
 		header('Location: ../index.php?mensaje=1');
-	}else{
-        $row = $result->fetch(PDO::FETCH_ASSOC);
+	} else {
+		$row = $result->fetch(PDO::FETCH_ASSOC);
 
 		session_regenerate_id();
-		$_SESSION['sess_user_id']  = $row['id'];
+		$_SESSION['sess_user_id'] = $row['id'];
+		$_SESSION['sess_usernom'] = $row['nombres'];
+		$_SESSION['sess_userapel'] = $row['apellidos'];
+		$_SESSION['sess_identificacion'] = $row['identificacion'];
 		$_SESSION['sess_username'] = $row['username'];
-        $_SESSION['sess_userrole'] = $row['role'];
-        $_SESSION['sess_usernom']  = $row['nombres'];
-        $_SESSION['sess_userapel'] = $row['apellidos'];
-		$_SESSION['sess_identificacion'] = $row['identificacion'];		
+		$_SESSION['sess_userrole'] = $row['role'];
 		session_write_close();
 
-		if( $_SESSION['sess_userrole'] == "1"){
+		if ($_SESSION['sess_userrole'] == $roles['administrador']) {
 			header('Location: ../administrador/home.php');
-		}elseif( $_SESSION['sess_userrole'] == "2"){
+		} elseif ($_SESSION['sess_userrole'] == $roles['docente']) {
 			header('Location: ../profesores/home.php');
-		}else{
-            header('Location: ../estudiantes/home.php');
-        }
-    }  
+		} else {
+			header('Location: ../estudiantes/home.php');
+		}
+	}
 
 
 ?>

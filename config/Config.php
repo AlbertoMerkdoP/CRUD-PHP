c7a387294abff0b38496cc5854a5f94e75aac215
AlbertoMerkdoP <jacksoon2004@gmail.com>
2023-08-14 23:04:53 -0500

    Agregar arrays de roles de usuarios y dias de semana

--- config/Config.php
+++ config/Config.php
@@ -5,8 +5,24 @@
 
     include_once('Funciones.php');
 
-    if ( !defined('ROOT')){
+    if (!defined('ROOT')) {
         define("ROOT", 'http://'.$_SERVER['HTTP_HOST'].getFolderProject());
     }
+
+    $roles = array(
+        'administrador' => 1,
+        'docente' => 2,
+        'estudiante' => 3,
+    );
+
+    $dias = array(
+        1 => 'Lunes',
+        2 => 'Martes',
+        3 => 'Miercoles',
+        4 => 'Jueves',
+        5 => 'Viernes',
+        6 => 'Sabado',
+        7 => 'Domingo'
+    )
 
 ?>

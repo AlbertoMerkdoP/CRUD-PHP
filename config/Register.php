8755d3c0bc2e86ee89e2a06dc3e6727f3f6ef95d
AlbertoMerkdoP <jacksoon2004@gmail.com>
2023-08-14 23:08:43 -0500

    Corregir conexión y validación de contraseñas

--- config/Register.php
+++ config/Register.php
@@ -2,23 +2,29 @@
 
 include_once("DBConect.php");
 
-if(isset($_POST['emailregis']))      $username       = $_POST['emailregis']; 
-if(isset($_POST['passwregis']))      $password       = md5($_POST['passwregis']);
-if(isset($_POST['nombres']))         $nombres        = $_POST['nombres'];
-if(isset($_POST['apellidos']))       $apellidos      = $_POST['apellidos'];
-if(isset($_POST['confirpassword']))  $confirpassword = md5($_POST['confirpassword']);
-if(isset($_POST['identificacion']))  $identificacion = $_POST['identificacion'];
+if (isset($_POST['emailregis']))
+   $username = $_POST['emailregis'];
+if (isset($_POST['password']))
+   $password = $_POST['password'];
+if (isset($_POST['nombres']))
+   $nombres = $_POST['nombres'];
+if (isset($_POST['apellidos']))
+   $apellidos = $_POST['apellidos'];
+if (isset($_POST['confirmPassword']))
+   $confirmPassword = $_POST['confirmPassword'];
+if (isset($_POST['identificacion']))
+   $identificacion = $_POST['identificacion'];
 
 $conexion = new Database;
-$result_consulta   = $conexion->ValidacionCorreo($username);
+$result_consulta = $conexion->ValidacionCorreo($username);
 $cantidad = $result_consulta->rowCount();
 
-if($cantidad > 0){
+if ($cantidad > 0) {
    header('Location: ../index.php?mensaje=3');
-}else{
-   $result = $conexion->Registrar($username,$password,$nombres,$apellidos,$identificacion);
+} elseif ($password == $confirmPassword) {
+   $result = $conexion->Registrar($username, $password, $nombres, $apellidos, $identificacion);
 
-   header('Location: ../index.php?mensaje='.$result);
+   header('Location: ../index.php?mensaje=' . $result);
 }
 
 
